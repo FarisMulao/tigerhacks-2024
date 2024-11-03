592997f3040e2039695dab66e70d843cca162c29
FarisMulao <143836772+FarisMulao@users.noreply.github.com>
2024-11-03 03:43:52 -0600

    proxy fix

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -10,7 +10,7 @@ module.exports = function (app) {
   );
 
   app.use(
-    "/upload",
+    "/uploadimage",
     createProxyMiddleware({
       target: "http://localhost:5000/",
       changeOrigin: true,
